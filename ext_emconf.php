4645de2c441963de44e715a3c46025d599cccd61
Deniz  Dagtekin <deniz.dagtekin@aoe.com>
2017-04-13 14:30:57 +0200

    Update typo3 dependencies to support TYPO3 7.6 in ext_emconf.php

--- ext_emconf.php
+++ ext_emconf.php
@@ -8,7 +8,7 @@ $EM_CONF[$_EXTKEY] = [
     'version' => '0.2.0',
     'constraints' => [
         'depends' => [
-            'typo3' => '6.2.0-7.2.99',
+            'typo3' => '6.2.0-7.6.99',
         ],
         'conflicts' => [],
         'suggests' => []
