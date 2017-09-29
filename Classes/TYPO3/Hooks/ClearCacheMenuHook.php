086d916f316f57d9e55fdd119773e1860343c1d4
Felix Semmler <felix.semmler@aoe.com>
2017-09-29 11:22:59 +0200

    [FEATURE] Make flush varnish cache button functional in TYPO3 7.6

--- Classes/TYPO3/Hooks/ClearCacheMenuHook.php
+++ Classes/TYPO3/Hooks/ClearCacheMenuHook.php
@@ -2,6 +2,7 @@
 namespace Aoe\Varnish\TYPO3\Hooks;
 
 use TYPO3\CMS\Backend\Toolbar\ClearCacheActionsHookInterface;
+use TYPO3\CMS\Backend\Utility\BackendUtility;
 use TYPO3\CMS\Lang\LanguageService;
 
 class ClearCacheMenuHook extends AbstractHook implements ClearCacheActionsHookInterface
@@ -15,11 +16,12 @@ class ClearCacheMenuHook extends AbstractHook implements ClearCacheActionsHookIn
         /** @var LanguageService $languageService */
         $languageService = $this->objectManager->get(LanguageService::class);
         $title = $languageService->sL('LLL:EXT:varnish/Resources/Private/Language/locallang.xlf:backendAjaxHook.title');
+
         $cacheActions[] = array(
             'id' => 'varnish',
             'title' => $title,
             //@todo change naming of ajax call "BAN:ALL"
-            'href' => 'ajax.php?ajaxID=varnish::BAN:ALL',
+            'href' =>  BackendUtility::getAjaxUrl('varnish::BAN:ALL', []),
             'icon' => '<img src="/' . $GLOBALS['TYPO3_LOADED_EXT']['varnish']['siteRelPath'] .
                 'ext_icon.gif" title="' . $title . '" alt="' . $title . '" />',
         );
--- ext_localconf.php
+++ ext_localconf.php
@@ -5,7 +5,11 @@ switch (TYPO3_MODE) {
         $TYPO3_CONF_VARS['SC_OPTIONS']['tslib/class.tslib_fe.php']['contentPostProc-output'][] = 'EXT:varnish/Classes/TYPO3/Hooks/ContentPostProcOutputHook.php:Aoe\\Varnish\\TYPO3\\Hooks\\ContentPostProcOutputHook->sendHeader';
         break;
     case 'BE':
-        $TYPO3_CONF_VARS['BE']['AJAX']['varnish::BAN:ALL'] = 'EXT:varnish/Classes/TYPO3/Hooks/BackendAjaxHook.php:Aoe\\Varnish\\TYPO3\\Hooks\\BackendAjaxHook->banAll';
+        \TYPO3\CMS\Core\Utility\ExtensionManagementUtility::registerAjaxHandler(
+            'varnish::BAN:ALL',
+            'Aoe\\Varnish\\TYPO3\\Hooks\\BackendAjaxHook->banAll'
+        );
+
         $TYPO3_CONF_VARS['SC_OPTIONS']['additionalBackendItems']['cacheActions'][] = 'EXT:varnish/Classes/TYPO3/Hooks/ClearCacheMenuHook.php:Aoe\\Varnish\\TYPO3\\Hooks\\ClearCacheMenuHook';
         $TYPO3_CONF_VARS['SC_OPTIONS']['t3lib/class.t3lib_tcemain.php']['clearCachePostProc'][] = 'EXT:varnish/Classes/TYPO3/Hooks/ContentPostProcOutputHook.php:Aoe\\Varnish\\TYPO3\\Hooks\\TceMainHook->clearCachePostProc';
         break;
