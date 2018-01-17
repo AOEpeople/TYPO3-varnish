a8cccb3d2d8499909e4fcd43f92657395a85ca6f
Felix Semmler <felix.semmler@gmail.com>
2018-01-17 09:12:18 +0100

    [TASK] Update travis and use nimut/testing-framework

--- Tests/Unit/TYPO3/Hooks/TceMainHookTest.php
+++ Tests/Unit/TYPO3/Hooks/TceMainHookTest.php
@@ -1,9 +1,35 @@
 <?php
-namespace Aoe\Varnish\TYPO3\Hooks;
+namespace Aoe\Varnish\Tests\Unit\TYPO3\Hooks;
+
+/***************************************************************
+ *  Copyright notice
+ *
+ *  (c) 2018 AOE GmbH <dev@aoe.com>
+ *
+ *  All rights reserved
+ *
+ *  This script is part of the TYPO3 project. The TYPO3 project is
+ *  free software; you can redistribute it and/or modify
+ *  it under the terms of the GNU General Public License as published by
+ *  the Free Software Foundation; either version 3 of the License, or
+ *  (at your option) any later version.
+ *
+ *  The GNU General Public License can be found at
+ *  http://www.gnu.org/copyleft/gpl.html.
+ *
+ *  This script is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *  GNU General Public License for more details.
+ *
+ *  This copyright notice MUST APPEAR in all copies of the script!
+ ***************************************************************/
 
 use Aoe\Varnish\Domain\Model\Tag\PageIdTag;
 use Aoe\Varnish\Domain\Model\Tag\PageTag;
 use Aoe\Varnish\System\Varnish;
+use Aoe\Varnish\TYPO3\Hooks\TceMainHook;
+use Nimut\TestingFramework\TestCase\UnitTestCase;
 use TYPO3\CMS\Core\Authentication\BackendUserAuthentication;
 use TYPO3\CMS\Core\DataHandling\DataHandler;
 use TYPO3\CMS\Extbase\Object\ObjectManagerInterface;
@@ -11,7 +37,7 @@ use TYPO3\CMS\Extbase\Object\ObjectManagerInterface;
 /**
  * @covers \Aoe\Varnish\TYPO3\Hooks\TceMainHook
  */
-class TceMainHookTest extends \PHPUnit_Framework_TestCase
+class TceMainHookTest extends UnitTestCase
 {
     /**
      * @var Varnish
